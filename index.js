072c5ab93339663ee101d86c3d773fe6f9d6cdf5
DeclanLacey <109444336+DeclanLacey@users.noreply.github.com>
2022-08-10 21:21:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const postImgOne = document.getElementById("post-one")
+const postImgTwo = document.getElementById("post-two")
+const postImgThree = document.getElementById("post-three")
+const postLikesOne = document.getElementById("post-likes-one")
+const postLikesTwo = document.getElementById("post-likes-two")
+const postLikesThree = document.getElementById("post-likes-three")
+
+console.log(postLikesOne)
+
+
+
+
